b57cb3863d57903da0107f345a76ff61f71fe84e
BradGamez <33333609+BradGamez@users.noreply.github.com>
2024-10-08 17:55:49 -0400

    Update constructors and added StorageManager

---
+++ Car.java
@@ -0,0 +1,69 @@
+public class Car {
+    private String Model;
+    private String Make;
+    private int Year;
+    private String VIN;
+    private String plateNum;
+    private String type;
+
+    public Car(String model, String make, int year, String VIN, String plateNum, String type) {
+        Model = model;
+        Make = make;
+        Year = year;
+        this.VIN = VIN;
+        this.plateNum = plateNum;
+        this.type = type;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+
+    public String getMake() {
+        return Make;
+    }
+
+    public void setMake(String make) {
+        Make = make;
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public void setYear(int year) {
+        Year = year;
+    }
+
+    public String getVIN() {
+        return VIN;
+    }
+
+    public void setVIN(String VIN) {
+        this.VIN = VIN;
+    }
+
+    public String getPlateNum() {
+        return plateNum;
+    }
+
+    public void setPlateNum(String plateNum) {
+        this.plateNum = plateNum;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String toString() {
+        return getModel() + "\n" + getMake() + "\n" + getYear() +  "\n" + getVIN() + "\n" + getPlateNum() + "\n" + getType();
+    }
+}
---
+++ StorageManager.java
@@ -0,0 +1,114 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class StorageManager {
+
+    public static void setCar(Car car) {
+        try {
+            File file = new File("./cars/" + car.getVIN() + ".txt");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter("./cars/" + car.getVIN() + ".txt");
+            fileWriter.write(car.toString());
+            fileWriter.close();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+    public static Car getCar(String vin) {
+        try {
+            LinkedList list = new LinkedList();
+            File file = new File("./cars/" + vin + ".txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            return new Car(list.get(0).toString(), list.get(1).toString(), Integer.parseInt(list.get(2).toString()), list.get(3).toString(), list.get(4).toString(), list.get(5).toString());
+        } catch (FileNotFoundException error) {
+            error.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void setClient(Client client) {
+        try {
+            File file = new File("./clients/" + client.getID() + ".txt");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter("./clients/" + client.getID() + ".txt");
+            fileWriter.write(client.toString());
+            fileWriter.close();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+    public static Client getClient(String clientID) {
+        try {
+            LinkedList list = new LinkedList();
+            File file = new File("./clients/" + clientID + ".txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            return new Client(list.get(0).toString(), list.get(1).toString(), Integer.parseInt(list.get(2).toString()), list.get(3).toString(), Double.valueOf(list.get(4).toString()));
+        } catch (FileNotFoundException error) {
+            error.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void setOwner(Owner owner) {
+        try {
+            File file = new File("./owners/" + owner.getID() + ".txt");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter("./owners/" + owner.getID() + ".txt");
+            fileWriter.write(owner.toString());
+            fileWriter.close();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+    public static Owner getOwner(String ownerID, String carVIN) {
+        try {
+            LinkedList list = new LinkedList();
+            File file = new File("./owners/" + ownerID + ".txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            return new Owner(list.get(0).toString(), list.get(1).toString(), Integer.parseInt(list.get(2).toString()), list.get(3).toString(), Double.valueOf(list.get(4).toString()), getCar(carVIN));
+        } catch (FileNotFoundException error) {
+            error.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void setJob(Job job) {
+        try {
+            File file = new File("./jobs/" + job.getID() + ".txt");
+            file.createNewFile();
+            FileWriter fileWriter = new FileWriter("./jobs/" + job.getID() + ".txt");
+            fileWriter.write(job.toString());
+            fileWriter.close();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+    public static Job getJob(String jobID) {
+        try {
+            LinkedList list = new LinkedList();
+            File file = new File("./jobs/" + jobID + ".txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                list.add(scanner.nextLine());
+            }
+            return new Job(list.get(0).toString(), list.get(1).toString(), Timestamp.valueOf(list.get(2).toString()), Double.valueOf(list.get(3).toString()));
+        } catch (FileNotFoundException error) {
+            error.printStackTrace();
+            return null;
+        }
+    }
+}
