a807bcd8c5914980e874f9e6245015e25fc43d2b
Hekatie <hekatie2003@gmail.com>
2024-10-08 14:33:10 -0400

    Create Client.java

---
+++ src/Client.java
@@ -0,0 +1,45 @@
+public class Client {
+    private String username;
+    private String password;
+    private int ID;
+    private String fullName;
+
+    public Client(String username, String password, int ID, String fullName) {
+        this.username = username;
+        this.password = password;
+        this.ID = ID;
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
